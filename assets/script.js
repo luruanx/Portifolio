416f3ce913b143669699032019beecbca8daf518
Ruan Richard <Richard_10luan@hotmail.com>
2025-02-11 02:20:53 -0300

    testemonials up

--- assets/script.js
+++ assets/script.js
@@ -62,3 +62,34 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+
+// TESTEMONIALS 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+    },
+  });
